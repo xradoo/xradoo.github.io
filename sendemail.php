8aa2d43d666b0546efac8afe1cc50e8bc301d820
DavyJon <codreanuradu93@gmail.com>
2019-04-12 16:14:27 +0300

    testing email sending

--- sendemail.php
+++ sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to         = 'trendytheme.net@gmail.com';  // put your email here
+    $to         = 'raducodreau93@gmail.com';  // put your email here
 
     $email_template = 'simple.html';
 
